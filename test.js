3ebbdec6969f0e09e7621e52f824466dd7e1ff62
项文俊 <xiangwenjun1@zhenhuipin.cn>
2022-01-27 17:27:46 +0800

    feat: 添加斐波那契算法

---
+++ test.js
@@ -0,0 +1,14 @@
+// 1=1 2=1 3=2  4=3  5=5 6=8 7=13 8=21 9=34
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+  const result = 1;
+  for (let i = 1; i < n; i++) {}
+  return result;
+};
+
+console.log(fib(1));
+console.log(fib(2));
