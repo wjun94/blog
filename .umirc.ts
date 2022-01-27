79de3786cd5ab15d8034126883a64738cc328732
项文俊 <xiangwenjun1@zhenhuipin.cn>
2022-01-27 10:22:42 +0800

    fix: 调整配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  title: 'Site Name',
-  mode: 'site',
+  title: '笔记',
+  mode: 'doc',
   // more config: https://d.umijs.org/config
 });
