74ebec826150e15b8c602ad7055c95525fd4b789
项文俊 <xiangwenjun1@zhenhuipin.cn>
2022-01-28 14:36:53 +0800

    feat: 添加数组的基本结构章节

--- .umirc.ts
+++ .umirc.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'dumi';
 
+console.log(process.env.NODE_ENV);
+
 export default defineConfig({
   title: '笔记',
   mode: 'doc',
-  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : './',
   // more config: https://d.umijs.org/config
 });
